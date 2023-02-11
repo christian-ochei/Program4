83763038865a6c4a80e0227f0111f59125f8b8c2
Christian <cj.lync@gmail.com>
2023-02-11 15:52:36 -0600

    Updated

--- p4.cpp
+++ p4.cpp
@@ -3,5 +3,68 @@
 //
 
 #include "p4.h"
+#include <string>
+
+using namespace std;
+
+sNode::sNode(string text){
+    this->text = text;
+}
+
+sBST::findMin(sNode * ptr){
+
+}
+
+sBST::insert(){
+
+}
+
+sBST::remove(){
+
+}
+
+sBST::isin(){
+
+}
+
+sBST::printIt(){
+
+}
+
+sBST::clear(){
+
+}
+
+// Public methods
+sBST::sBST(){
+
+}
+
+sBST::~sBST(){
+
+}
+
+bool sBST::insert(string text){
+
+}
+
+bool sBST::remove(string text){
+
+}
+
+bool sBST::isin(string text){
+
+}
+
+void sBST::printIt() const{
+
+}
+
+int sBST::count() const{
+
+}
 
 
+void sBST::clear(){
+
+}
