e49dea635b5389035a596ba689a5d9586410e464
Christian <cj.lync@gmail.com>
2023-02-11 15:42:57 -0600

    Updated

---
+++ p4.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 12145 on 2/11/2023.
+//
+
+#include "p4.h"
---
+++ p4.h
@@ -0,0 +1,14 @@
+//
+// Created by 12145 on 2/11/2023.
+//
+
+#ifndef PROGRAM4_P4_H
+#define PROGRAM4_P4_H
+
+
+class p4 {
+
+};
+
+
+#endif //PROGRAM4_P4_H
