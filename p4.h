0f776f2df762f7587b400f04bc1853da20e60fa1
habtamu-heyi <124469577+habtamu-heyi@users.noreply.github.com>
2023-02-11 16:00:11 -0600

    asdf

--- p4.h
+++ p4.h
@@ -4,30 +4,24 @@
 
 #ifndef PROGRAM4_P4_H
 #define PROGRAM4_P4_H
-
-Class sNode{
-  Private: 
-  string text;
+#include<string>
+class sNode{
+  private: 
+  std::string text;
   int h;
   sNode* left, *right;   // left and right children pointers
 };
 class sBST{
-Private: 
+private: 
   int treeCount;
   sNode*root;
-  string findMin(sNode *ptr);
-   insert(sNode* p); 
-   remove(sNode* text);
-   isIn();
-   printIt();
-   clear(sNode* p);
-  
-Private members:
-An integer “treeCount” to record the number of entries in the BST
- An sNode *root; to point to the tree of nodes.
- string findMin(sNode *ptr) which returns the minimum value in the subtree whose root node
-is pointed to by ptr.
- insert(), remove(), isIn(), printIt(), and clear().
+  std::string findMin(sNode *ptr);
+  bool insert(sNode* p); 
+  bool remove(sNode* text);
+  int isIn();
+  void printIt();
+  void  clear(sNode* p);
+ 
 };
 
-#endif //PROGRAM4_P4_H
+
