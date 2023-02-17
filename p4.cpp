324db6e84f79df15fad69660b78f15b431906ff4
habtamu-heyi <124469577+habtamu-heyi@users.noreply.github.com>
2023-02-17 02:29:17 -0600

    Update p4.cpp

--- p4.cpp
+++ p4.cpp
@@ -54,6 +54,72 @@ bool sBST::insert(string text, sNode*& node){
     }
     return retVal;
 }
+
+bool sBST::remove(sNode* p, string text) {
+    bool rc = false;
+    if (p) {
+        // Check if the node passed is less than the root
+        if (text < p->text) {
+            // If so, remove left through recursion
+            rc = remove(p->left, text);
+        }
+        // Else if the node value to be deleted is greater than that of the root node
+        else if (text > p->text) {
+            // Remove the right child value
+            rc = remove(p->right, text);
+        }
+        // If the tree has only a right child
+        else if (p->left == nullptr) {
+            sNode* temp = p->right;
+            delete p;
+            p = temp;
+            rc = true;
+        }
+        // If the tree has only a left child
+        else if (p->right == nullptr) {
+            sNode *temp = p->left;
+            delete p;
+            p = temp;
+            rc = true;
+        }
+        else {
+            // If the tree has two children
+            sNode *temp = p->right;
+            while (temp->left) {
+                temp = temp->left;
+            }
+
+            p->text = temp->text;
+            rc = remove(p->right, temp->text);
+        }
+    }
+    return rc;
+}
+bool sBST::isIn(sNode* p, string text)  {
+    // If the node is null, then the text is not in the tree
+    if (!p) {
+        return false;
+    }
+
+    // If the text is less than the current node's text, search the left subtree
+    if (text < p->text) {
+        return isIn(p->left, text);
+    }
+    // If the text is greater than the current node's text, search the right subtree
+    else if (text > p->text) {
+        return isIn(p->right, text);
+    }
+    // If the text matches the current node's text, then it's in the tree
+    else {
+        return true;
+    }
+}
+
+bool sBST::isIn(string text) const {
+    // Recursively check if the text is in the tree, starting from the root node
+    return isIn(root, text);
+}
+
 // Author: Christian Ochei and Habtamu Wario
 void sBST::printIt(sNode* node) const{
     if (node){
